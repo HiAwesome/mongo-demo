460b725714885f776e1579c925b4b4affc1cd4dc
moqi <moqimoqidea@gmail.com>
2021-01-08 20:44:32 +0800

    建立 mongod replSet

---
+++ ali/shell/01_mkdir_3.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+# 首先建立三个子文件夹
+mkdir ./../mongoRs
+mkdir ./../mongoRs/rs1 ./../mongoRs/rs2 ./../mongoRs/rs3
+echo "create 3 sub dir success"
---
+++ ali/shell/02_start_3_mongod.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+# 启动三个 MongoDB 服务
+# 由于 27017 端口已被 brew 默认启动占用，以 27018 开始
+mongod --replSet rs --dbpath ./../mongoRs/rs1 --port 27018 --fork --logpath ./../mongoRs/rs1/mongod.log
+
+mongod --replSet rs --dbpath ./../mongoRs/rs2 --port 27019 --fork --logpath ./../mongoRs/rs2/mongod.log
+
+mongod --replSet rs --dbpath ./../mongoRs/rs3 --port 27020 --fork --logpath ./../mongoRs/rs3/mongod.log
+echo "start 3 mongod success"
